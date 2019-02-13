48488899bc82ca6f9d9a41cc42a13743892fd876
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-02-13 14:32:21 -0800

    Base funcitonality of MVP is done. Adding styling now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
+
+import './app.css';
+
+const taskArray = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state = {
+      taskArray: taskArray,
+      taskInput: ''
+    }
+  };
+
+  addTask = e => {
+    e.preventDefault();
+
+    const newTask = {
+      task: this.state.taskInput,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      taskArray: [...this.state.taskArray, newTask],
+    });
+  };
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="app-container">
+        <h1>Todo List</h1>
+        <TodoList taskArray={this.state.taskArray} />
+        <TodoForm addTask={this.addTask} taskInput={this.state.taskInput} handleChanges={this.handleChanges} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,12 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = (props) => (
+  <ul className="todo-list">
+    {props.taskArray.map((taskOfMap, index) => (
+      <Todo key={index} task={taskOfMap.task} />
+    ))}
+  </ul>
+);
+
+export default TodoList;
